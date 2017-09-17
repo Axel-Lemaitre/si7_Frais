439880fa8434e9662840a384b8e47312d660d4cf
MaelleT <maelle.taurand@ac-nantes.fr>
2017-09-17 21:15:10 +0200

    Initial commit

---
+++ genererPDF.php
@@ -0,0 +1,110 @@
+<?php
+require('fpdf/fpdf.php');
+
+$pdf = new FPDF();
+$pdf->AddPage();
+$pdf->SetFont('Arial','B',14);
+//$pdf->Cell(40,10,'Hello World !');
+$repInclude = './include/';
+require($repInclude . "_init.inc.php");
+
+  // page inaccessible si visiteur non connecté
+  if ( ! estVisiteurConnecte() ) {
+      header("Location: cSeConnecter.php");  
+  }
+  
+  $moisSaisi=lireDonnee("mois", "");
+  $tabFicheFrais = obtenirDetailFicheFrais($idConnexion, $moisSaisi, obtenirIdUserConnecte());
+
+
+// affichage des différents éléments (forfaitisés et non forfaitisés)
+// de la fiche de frais demandée, uniquement si pas d'erreur détecté au contrôle
+$lMois=obtenirLibelleMois(intval(substr($moisSaisi,4,2)));
+$pdf->Write(5,'Fiche de frais du mois de '.$lMois." ".substr($moisSaisi,0,4));
+$pdf->Ln(); 
+$pdf->Write(5,$tabFicheFrais["libelleEtat"].' depuis le '.$tabFicheFrais["dateModif"]);
+/*
+    <div class="encadre">
+    <p>Montant validé : <?php echo $tabFicheFrais["montantValide"] ;
+        ?>              
+    </p>
+<?php          
+            // demande de la requête pour obtenir la liste des éléments 
+            // forfaitisés du visiteur connecté pour le mois demandé
+            $req = obtenirReqEltsForfaitFicheFrais($moisSaisi, obtenirIdUserConnecte());
+            $idJeuEltsFraisForfait = mysql_query($req, $idConnexion);
+            echo mysql_error($idConnexion);
+            $lgEltForfait = mysql_fetch_assoc($idJeuEltsFraisForfait);
+            // parcours des frais forfaitisés du visiteur connecté
+            // le stockage intermédiaire dans un tableau est nécessaire
+            // car chacune des lignes du jeu d'enregistrements doit être doit être
+            // affichée au sein d'une colonne du tableau HTML
+            $tabEltsFraisForfait = array();
+            while ( is_array($lgEltForfait) ) {
+                $tabEltsFraisForfait[$lgEltForfait["libelle"]] = $lgEltForfait["quantite"];
+                $lgEltForfait = mysql_fetch_assoc($idJeuEltsFraisForfait);
+            }
+            mysql_free_result($idJeuEltsFraisForfait);
+            ?>
+  	<table class="listeLegere">
+  	   <caption>Quantités des éléments forfaitisés</caption>
+        <tr>
+            <?php
+            // premier parcours du tableau des frais forfaitisés du visiteur connecté
+            // pour afficher la ligne des libellés des frais forfaitisés
+            foreach ( $tabEltsFraisForfait as $unLibelle => $uneQuantite ) {
+            ?>
+                <th><?php echo $unLibelle ; ?></th>
+            <?php
+            }
+            ?>
+        </tr>
+        <tr>
+            <?php
+            // second parcours du tableau des frais forfaitisés du visiteur connecté
+            // pour afficher la ligne des quantités des frais forfaitisés
+            foreach ( $tabEltsFraisForfait as $unLibelle => $uneQuantite ) {
+            ?>
+                <td class="qteForfait"><?php echo $uneQuantite ; ?></td>
+            <?php
+            }
+            ?>
+        </tr>
+    </table>
+  	<table class="listeLegere">
+  	   <caption>Descriptif des éléments hors forfait - <?php echo $tabFicheFrais["nbJustificatifs"]; ?> justificatifs reçus -
+       </caption>
+             <tr>
+                <th class="date">Date</th>
+                <th class="libelle">Libellé</th>
+                <th class="montant">Montant</th>                
+             </tr>
+<?php          
+            // demande de la requête pour obtenir la liste des éléments hors
+            // forfait du visiteur connecté pour le mois demandé
+            $req = obtenirReqEltsHorsForfaitFicheFrais($moisSaisi, obtenirIdUserConnecte());
+            $idJeuEltsHorsForfait = mysql_query($req, $idConnexion);
+            $lgEltHorsForfait = mysql_fetch_assoc($idJeuEltsHorsForfait);
+            
+            // parcours des éléments hors forfait 
+            while ( is_array($lgEltHorsForfait) ) {
+            ?>
+                <tr>
+                   <td><?php echo $lgEltHorsForfait["date"] ; ?></td>
+                   <td><?php echo filtrerChainePourNavig($lgEltHorsForfait["libelle"]) ; ?></td>
+                   <td><?php echo $lgEltHorsForfait["montant"] ; ?></td>
+                </tr>
+            <?php
+                $lgEltHorsForfait = mysql_fetch_assoc($idJeuEltsHorsForfait);
+            }
+            mysql_free_result($idJeuEltsHorsForfait);
+  ?>
+    </table>
+  </div>
+  </div>
+<?php        
+//  require($repInclude . "_pied.inc.html");
+//  require($repInclude . "_fin.inc.php");
+*/
+$pdf->Output();
+?> 
